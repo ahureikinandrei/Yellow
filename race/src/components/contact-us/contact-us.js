83636fd20d514560906eac1de88e0f3ff1a48773
ANDREI AHUREIKIN <ahureikinandrei@gmail.com>
2020-10-09 15:12:31 +0300

    feat: contact us info

--- race/src/components/contact-us/contact-us.js
+++ race/src/components/contact-us/contact-us.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 import './contact-us.css';
+import photo from './My-photo.jpg';
 
 const ContactUs = () => {
     return (
-        <div >
+        <div className="contact-us">
+            <img src={photo} alt="My photo" className="contact-us__photo" />
+            <a href="https://www.linkedin.com/in/andrei-ahureikin/" target="_blank">Andrei Ahureikin</a>
         </div>
     );
 };
